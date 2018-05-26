0ff23ff516488a58428904d631d2941ea99fb748
Garry T <incarnate@rogers.com>
2018-05-25 20:17:24 -0400

    Added Selected indicator, Search option implemented, trying to work on drawing a line from point to point.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { isMobile } from 'react-device-detect';
 import axios from 'axios';
+import classNames from 'classnames';
 import {
   ComposableMap,
   ZoomableGroup,
@@ -27,20 +28,23 @@ export default class Map extends Component {
     this.state = {
       center: [0, 0],
       zoom: 1,
-      flights: []
+      flights: [],
+      selectedFlight: '',
+      callsign: ''
     };
 
+    this.interval = null;
     this.handleZoomIn = this.handleZoomIn.bind(this)
     this.handleZoomOut = this.handleZoomOut.bind(this)
     this.handleFlightClick = this.handleFlightClick.bind(this)
     this.handleReset = this.handleReset.bind(this)
+    this.findFlight = this.findFlight.bind(this)
+    this.flightInputChange = this.flightInputChange.bind(this);
   }
 
   handleZoomIn() {
-    console.log(this.state);
     this.setState({
-      // center: [174.33353257621044, -35.002372900345634],
-      zoom: this.state.zoom * 2
+      zoom: this.state.zoom * 3
     })
   }
 
@@ -57,15 +61,31 @@ export default class Map extends Component {
 
     this.setState({
       center: flight.coordinates,
-      zoom: 8
+      selectedFlight: flight,
+      zoom: this.state.zoom === 1 ? 50 : this.state.zoom
     })
   }
 
   handleReset() {
     this.setState({
       center: [0, 0],
+      selectedFlight: '',
       zoom: 1
     })
+  }
+
+  findFlight() {
+    if (this.state.flights.length > 0) {
+      const flight = this.state.flights.find(flight => {
+        return flight.callsign.toUpperCase() === this.state.callsign.toUpperCase()
+      })
+
+      if (flight) this.handleFlightClick(flight);
+    }
+  }
+
+  flightInputChange(e) {
+    this.setState({ callsign: e.target.value });
   }
 
   async getData() {
@@ -88,6 +108,7 @@ export default class Map extends Component {
         if (!this.checkFlightPosition(clientInterface)) {
           flightDataArr.push({
             name: clientInterface.realname,
+            callsign: clientInterface.callsign,
             coordinates: [parseFloat(clientInterface.longitude), parseFloat(clientInterface.latitude)],
             frequency: clientInterface.frequency,
             altitude: clientInterface.altitude,
@@ -104,13 +125,39 @@ export default class Map extends Component {
     })
   }
 
+  startInterval() {
+		this.interval = setInterval(() => {
+			this.getFlightData();
+		}, 60000);
+	}
+
   checkFlightPosition(clientInterface) {
     return ((isNaN(clientInterface.longitude) || clientInterface.longitude === '') ||
             (isNaN(clientInterface.latitude) || clientInterface.latitude === ''));
   }
 
+  assignPlaneClass(marker) {
+    let selectedFlightClass = null;
+
+    if (marker.callsign === this.state.selectedFlight.callsign) {
+      selectedFlightClass = classNames(
+        'plane',
+        '--selected'
+      );
+    } else {
+      selectedFlightClass = classNames(
+        'plane'
+      );
+    }
+
+    return selectedFlightClass;
+  }
+
   componentDidMount() {
-    this.getFlightData();
+    if (!this.interval) {
+      this.startInterval();
+      this.getFlightData();
+    }
   }
 
   // projection() {
@@ -125,6 +172,8 @@ export default class Map extends Component {
   // }
 
   render() {
+    let selectedFlightClass = null;
+
     return (
       <div style={wrapperStyles}>
         <button onClick={this.handleZoomIn}>
@@ -136,6 +185,15 @@ export default class Map extends Component {
         <button onClick={this.handleReset}>
           { "Reset" }
         </button>
+        <input
+          type="text"
+          placeholder="Search for the callsign..."
+          onChange={this.flightInputChange} />
+        <input
+          type="button"
+          value="Search"
+          onClick={this.findFlight}
+        />
         <Motion
           defaultStyle={{
             zoom: 1,
@@ -143,16 +201,16 @@ export default class Map extends Component {
             y: 20,
           }}
           style={{
-            zoom: spring(this.state.zoom, {stiffness: 210, damping: 20}),
-            x: spring(this.state.center[0], {stiffness: 210, damping: 20}),
-            y: spring(this.state.center[1], {stiffness: 210, damping: 20}),
+            zoom: spring(this.state.zoom, {stiffness: 180, damping: 20}),
+            x: spring(this.state.center[0], {stiffness: 100, damping: 20}),
+            y: spring(this.state.center[1], {stiffness: 100, damping: 20}),
           }}
           >
           {({zoom,x,y}) => (
               <ComposableMap
                 projectionConfig={{ scale: 205 }}
                 width={980}
-                height={551}
+                height={800}
                 style={{
                   width: "100%",
                   height: "auto",
@@ -191,18 +249,25 @@ export default class Map extends Component {
                     ))}
                   </Geographies>
                   <Markers>
-                    {this.state.flights.map((marker, i) => (
-                      <Marker
-                        key={i}
-                        marker={marker}
-                        onClick={this.handleFlightClick}
-                        rotate={marker.heading}>
-                        <path
-                          d={PLANE_SVG_PTH}
-                          strokeWidth="0"
-                          fill="#73b6e6" />
-                      </Marker>
-                    ))}
+                    {this.state.flights.map((marker, i) => {
+                      selectedFlightClass = this.assignPlaneClass(marker);
+
+                      return (
+                        <Marker
+                          key={i}
+                          marker={marker}
+                          onClick={this.handleFlightClick}
+                          rotate={marker.heading}
+                          defaultZoomNumerator={isMobile ? "0.50" : "0.35"}>
+                          <path
+                            className={selectedFlightClass}
+                            d={PLANE_SVG_PTH}
+                            strokeWidth="0"
+                            fill="#73b6e6" />
+                          <line x1="0" y1="0" x2="200" y2="200" style={{stroke:"rgb(255,0,0)", strokeWidth: "1"}} />
+                        </Marker>
+                      )
+                    })}
                   </Markers>
                 </ZoomableGroup>
               </ComposableMap>
