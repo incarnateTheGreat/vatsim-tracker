992cccc008cc756fcfe8ecd0ab0b2cbc4902bd68
Garry T <incarnate@rogers.com>
2018-05-28 15:26:42 -0400

    Rendering Icons with VATSIM data successfully.

--- src/components/MapLeaflet.js
+++ src/components/MapLeaflet.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import {
   Map,
   TileLayer,
   Marker,
   Popup } from 'react-leaflet'
-import { CITIES } from '../constants/constants'
+import { CLIENT_LABELS, CITIES } from '../constants/constants'
 
 export default class MapLeaflet extends Component {
   state = {
@@ -13,8 +14,10 @@ export default class MapLeaflet extends Component {
     zoom: 3,
     height: 1000,
     width: 500,
-    markers: []
+    flights: []
   }
+
+  interval = null;
 
   getViewportSize() {
     const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
@@ -33,32 +36,85 @@ export default class MapLeaflet extends Component {
     })
   }
 
-  addMarker = (lat, lng) => {
-    const { markers } = this.state
-    markers.push([lat, lng])
-    this.setState({ markers })
+  startInterval() {
+		this.interval = setInterval(() => {
+			this.getFlightData();
+		}, 60000);
+	}
+
+  checkFlightPosition(clientInterface) {
+    return ((isNaN(clientInterface.longitude) || clientInterface.longitude === '') ||
+            (isNaN(clientInterface.latitude) || clientInterface.latitude === ''));
+  }
+
+  async getData() {
+    return await axios('http://localhost:8000/api/vatsim-data')
+      .then(res => res.data);
+  }
+
+  getFlightData() {
+    let flightDataArr = [];
+
+    this.getData().then(data => {
+      for (let i = 0; i < data.length; i++) {
+        let clientInterface = {},
+            clientDataSplit = data[i].split(':');
+
+        for (let j = 0; j < CLIENT_LABELS.length; j++) {
+          clientInterface[CLIENT_LABELS[j]] = clientDataSplit[j];
+        }
+
+        if (!this.checkFlightPosition(clientInterface)) {
+          flightDataArr.push({
+            name: clientInterface.realname,
+            callsign: clientInterface.callsign,
+            coordinates: [parseFloat(clientInterface.longitude), parseFloat(clientInterface.latitude)],
+            frequency: clientInterface.frequency,
+            altitude: clientInterface.altitude,
+            planned_aircraft: clientInterface.planned_aircraft,
+            heading: clientInterface.heading,
+            groundspeed: clientInterface.groundspeed,
+            transponder: clientInterface.transponder,
+            planned_depairport: clientInterface.planned_depairport,
+            planned_destairport: clientInterface.planned_destairport
+          })
+        }
+      }
+
+      this.setState({ flights: flightDataArr });
+    })
+  }
+
+  addFlight = (name, lat, lng) => {
+    const { flights } = this.state
+    flights.push({ name: name, coordinates: [lat, lng] })
+    this.setState({ flights }, () => {
+      console.log(this.state);
+    })
   }
 
   componentDidMount = () => {
     const { width, height } = this.getViewportSize()
 
-    for (let x = 0; x < CITIES.length; x++) {
-      this.addMarker(CITIES[x].coordinates[1], CITIES[x].coordinates[0])
-    }
+    // for (let x = 0; x < CITIES.length; x++) {
+    //   this.addFlight(CITIES[x].name, CITIES[x].coordinates[1], CITIES[x].coordinates[0])
+    // }
 
     setTimeout(() => {
       this.setState({ width, height }, () => {
-        this.setResizeEvent();
-        window.dispatchEvent(new Event('resize'));
+        if (!this.interval) {
+          this.setResizeEvent();
+          this.getFlightData();
+          this.startInterval();
+          window.dispatchEvent(new Event('resize'));
+        }
       })
     }, 0);
   }
 
   render() {
-    // const position = [this.state.lat, this.state.lng]
-
     return (
-      <div id="mapid">
+      <div>
         <Map
           center={[this.state.lat, this.state.lng]}
           zoom={this.state.zoom}
@@ -70,16 +126,39 @@ export default class MapLeaflet extends Component {
             attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {this.state.markers.map((position, idx) =>
+        {this.state.flights.map((position, idx) => {
+          const { name,
+                  callsign,
+                  altitude,
+                  heading,
+                  groundspeed,
+                  transponder,
+                  planned_depairport,
+                  planned_destairport,
+                  planned_aircraft } = position
+
+          const coords = [position.coordinates[1], position.coordinates[0]]
+
+          return (
             <Marker
               key={`marker-${idx}`}
-              position={position}
+              position={coords}
             >
               <Popup>
-                <span>eee</span>
+                <div>
+                  <div><strong>{callsign}</strong></div>
+                  <div>{name}</div>
+                  <div>{planned_aircraft}</div>
+                  <div>{altitude} FT.</div>
+                  <div>{groundspeed} KTS</div>
+                  <div>{transponder}</div>
+                  <div>{planned_depairport} -> {planned_destairport}</div>
+                </div>
               </Popup>
-            </Marker>)
-          }
+            </Marker>
+          )
+        })
+        }
         </Map>
       </div>
     )
