4902baf67b2e6386a5529004abe82e56bee1d948
Garry T <incarnate@rogers.com>
2018-08-01 18:57:01 -0400

    Map and data: Still working on displaying Windroses. Updated airports.json

--- src/components/VatsimMap.js
+++ src/components/VatsimMap.js
@@ -267,15 +267,115 @@ export default class VatsimMap extends Component {
           rain = Leaflet.OWM.rain(options),
           clouds = Leaflet.OWM.clouds(options),
           precipitation = Leaflet.OWM.precipitation(options),
-          wind = Leaflet.OWM.wind(options),
+          windrose = Leaflet.OWM.current({
+            appId: 'a22e7429bbe086045388e31142cd915f',
+            intervall: 15,
+            minZoom: 3,
+            lang: 'EN',
+            markerFunction: this.myWindroseMarker,
+            popup: false,
+            clusterSize: 50,
+            imageLoadingBgUrl: 'https://openweathermap.org/img/w0/iwind.png'
+          }),
           overlayMaps = {
             'Rain': rain,
             'Clouds': clouds,
             'Precipitation': precipitation,
-            'Wind': wind },
-          baseMaps = {};
+            'Wind Rose': windrose
+          },
+          baseMaps = {}
 
-    const layerControl = Leaflet.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(this.map);
+    // windrose.on('owmlayeradd', this.windroseAdded, windrose)
+
+    Leaflet.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(this.map)
+  }
+
+  myWindroseMarker = data => {
+  	const content = '<canvas id="id_' + data.id + '" width="50" height="50" style="border: 1px solid red"></canvas>',
+          icon = Leaflet.divIcon({html: content, iconSize: [50,50], className: 'owm-div-windrose'});
+
+  	return Leaflet.marker([data.coord.Lat, data.coord.Lon], {icon: icon, clickable: false});
+  }
+
+  windroseAdded = e => {
+  	for (var i in this.map._layers) {
+  		var m = this.map._layers[i];
+  		var cv = document.getElementById('id_' + m.options.owmId);
+      console.log(m.options.owmId);
+  		for (var j in this._cache._cachedData.list) {
+  			var station = this._cache._cachedData.list[j];
+  			if (station.id === m.options.owmId) {
+  				this.myWindroseDrawCanvas(station, this);
+  			}
+  		}
+  	}
+  }
+
+  myWindroseDrawCanvas = (data, owm) => {
+    console.log('myWindroseDrawCanvas');
+  	var canvas = document.getElementById('id_' + data.id);
+  	canvas.title = data.name;
+  	var angle = 0;
+  	var speed = 0;
+  	var gust = 0;
+  	if (typeof data.wind !== 'undefined') {
+  		if (typeof data.wind.speed !== 'undefined') {
+  			canvas.title += ', ' + data.wind.speed + ' m/s';
+  			canvas.title += ', ' + owm._windMsToBft(data.wind.speed) + ' BFT';
+  			speed = data.wind.speed;
+  		}
+  		if (typeof data.wind.deg !== 'undefined') {
+  			canvas.title += ', ' + owm._directions[(data.wind.deg/22.5).toFixed(0)];
+  			angle = data.wind.deg;
+  		}
+  		if (typeof data.wind.gust !== 'undefined') {
+  			gust = data.wind.gust;
+  		}
+  	}
+  	if (canvas.getContext && speed > 0) {
+  		var red = 0;
+  		var green = 0;
+  		if (speed <= 10) {
+  			green = 10*speed+155;
+  			red = 255*speed/10.0;
+  		} else {
+  			red = 255;
+  			green = 255-(255*(Math.min(speed, 21)-10)/11.0);
+  		}
+  		var ctx = canvas.getContext('2d');
+  		ctx.translate(25, 25);
+  		ctx.rotate(angle*Math.PI/180);
+  		ctx.fillStyle = 'rgb(' + Math.floor(red) + ',' + Math.floor(green) + ',' + 0 + ')';
+  		ctx.beginPath();
+  		ctx.moveTo(-15, -25);
+  		ctx.lineTo(0, -10);
+  		ctx.lineTo(15, -25);
+  		ctx.lineTo(0, 25);
+  		ctx.fill();
+
+  		// draw inner arrow for gust
+  		if (gust > 0 && gust !== speed) {
+  			if (gust <= 10) {
+  				green = 10*gust+155;
+  				red = 255*gust/10.0;
+  			} else {
+  				red = 255;
+  				green = 255-(255*(Math.min(gust, 21)-10)/11.0);
+  			}
+  			canvas.title += ', gust ' + data.wind.gust + ' m/s';
+  			canvas.title += ', ' + owm._windMsToBft(data.wind.gust) + ' BFT';
+  			ctx.fillStyle = 'rgb(' + Math.floor(red) + ',' + Math.floor(green) + ',' + 0 + ')';
+  			ctx.beginPath();
+  			ctx.moveTo(-15, -25);
+  			ctx.lineTo(0, -10);
+  			ctx.lineTo(0, 25);
+  			ctx.fill();
+  		}
+  	} else {
+  		canvas.innerHTML = '<div>'
+  				+ (typeof data.wind !== 'undefined' && typeof data.wind.deg !== 'undefined' ? data.wind.deg + '°' : '')
+  				+ '</div>';
+  	}
   }
 
   handleEnterKey = (e) => {
@@ -477,13 +577,6 @@ export default class VatsimMap extends Component {
       })
     }, 0)
   }
-
-  // myWindroseMarker = (data) => {
-  // 	const content = '<canvas id="id_' + data.id + '" width="50" height="50"></canvas>',
-  //         icon = Leaflet.divIcon({html: content, iconSize: [50,50], className: 'owm-div-windrose'});
-  //
-  // 	return Leaflet.marker([data.coord.Lat, data.coord.Lon], {icon: icon, clickable: false});
-  // }
 
   render = () => {
     return (
